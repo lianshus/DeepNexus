77df4becbdd4130d66e812b45a680c2487440726
lianshus <2926786562@qq.com>
2024-11-01 23:50:08 +0800

    bg1

--- fronted/app/more/page.tsx
+++ fronted/app/more/page.tsx
@@ -10,7 +10,7 @@ export default function More() {
         position: "absolute",
         zIndex: 1,
         width: '100%'
-      } } 
+      } as any } 
       />
         <div className="w-full h-full flex flex-col justify-center items-center min-h-screen z-0">
           <div className="w-full max-w-7xl text-center text-9xl">
--- fronted/app/page.tsx
+++ fronted/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         position: "absolute",
         zIndex: 1,
         width: '100%'
-      }} 
+      } as any} 
       />
         <div className="w-full h-full flex flex-col justify-center min-h-screen z-0">
           <div className="mt-16 ml-40 w-full max-w-7xl text-left">
